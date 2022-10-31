d169a85421793fda4c8e78c1a7a7fe44ae226a75
bradyrye <bradyryehansen@gmail.com>
2022-10-31 12:29:11 -0600

    changes

--- Homework9/Sketch/stetch.js
+++ Homework9/Sketch/stetch.js
@@ -5,5 +5,29 @@ function setup()
 
 function draw()
 {
-    background(120,45,78);
+    background(20,45,78);
+    fill(300, 138, 9);
+    circle(250,250,300);
+
+    fill(200,40,76);
+    rect(230,250,30,40);
+    fill(100,0,0);
+    circle(210,200,60);
+    circle(310,200,60)
+    fill(51);
+    circle(210,200,30);;
+    circle(310,200,30);
+  fill(300);
+   stroke(255);
+  point(210, 300);
+  point(310, 200);
+  stroke(4000);
+  line(250, 100, 140, 210);
+  line(250, 100, 270, 210);
+  line(250, 30, 250, 100);
+  line(275,300,250,200);
+  line(275,300,200,300);
+  fill(300,40,5)
+  triangle(150,350,350,350,250,390);
 }
+
